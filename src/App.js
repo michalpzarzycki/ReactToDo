0abe18dc4d192d1f4a0566910ebd9b4722c9d020
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-14 00:44:18 +0100

    Created first more-less view style in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ItemsList from './components/ItemsList'
+import Input from './components/Input'
+import Button from './components/Button'
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <div className="addSection">
+        <Input />
+        <Button />
+      </div>
+      <div className="itemsLists">
        <ItemsList />
        <ItemsList />
+      </div>
     </div>
   );
 }
