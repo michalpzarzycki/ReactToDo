25811a4e4d565ff545c396aeecae60c83040e8ad
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-14 01:04:43 +0100

    Set the state in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ItemsList from './components/ItemsList'
 import AddItem from './components/AddItem'
 import './App.css';
 
 const App = () => {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+  const [id, setId] = useState("");
+  const [date, setDate] = useState(new Date().toLocaleDateString());
+  const [value, setValue] = useState("1");
+  const [deadline, setDeadline] = useState(new Date());
+  const [color, setColoe] = useState("red");
+
   return (
     <div className="App">
       <div className="addSection">
