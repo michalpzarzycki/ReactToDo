3dce8bc18703c641bf2dce95553a52f281aeec9a
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-14 01:15:11 +0100

    Created all needed methods: handleChange, handleSelectChange, handleDateChange, handleSubmit, deleteAllList, itemDelete

--- src/App.js
+++ src/App.js
@@ -11,7 +11,58 @@ const App = () => {
   const [value, setValue] = useState("1");
   const [deadline, setDeadline] = useState(new Date());
   const [color, setColoe] = useState("red");
+  
+  const handleChange = (e) => {
+    console.log("e", e)
+    setItem(e.target.value)
+  }
+  const handleSelectChange = (e) => {
+    console.log("e", e)
+    setValue(e.target.value)
+  }
+  const handleDateChange = (date) => {
+    console.log("e", date.toString())
+    setDeadline(date.toString())
+  }
+  const handleSubmit = (e) => {
+    console.log("VALUE W SUBMICIE: ", value)
+    console.log("DEADLINE W SUBMICIE", deadline)
+    e.preventDefault();
+    setDate( new Date().toLocaleString())
 
+    console.log("DEADLINE:w subic", deadline)
+
+
+    const newItem = {
+      item: item,
+      id: id,
+      date: date,
+      value: value,
+      deadline: deadline.toLocaleString().toString()
+    }
+
+    console.log(newItem)
+    if (newItem.item) {
+
+      const updatedItems = [...items, newItem]
+setItems(updatedItems);
+setItem("");
+setDate(new Date().toLocaleString());
+setValue("1");
+setDeadline(new Date())
+     
+    }
+  }
+  const deleteAllList = () => {
+    setItems([])
+  }
+  const itemDelete = (id) => {
+    let deletedItem = items.filter(item => (item.id == id))
+    console.log("DELETED ITEM: ", deletedItem)
+
+    const filteredItems = items.filter(item => (item.id !== id))
+    setItems(filteredItems)
+  }
   return (
     <div className="App">
       <div className="addSection">
