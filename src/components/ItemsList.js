c1c89e824c83c9aa5d2a6620940d1ec132582830
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-18 18:42:26 +0100

    Added date picker

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -4,13 +4,17 @@ import styles from './ItemsList.module.css'
 
 const ItemsList = (props) => {
     const { items, deleteAllList, itemDelete, itemDone } = props;
+    console.log("ITEMS LENGTH", items.length)
+    
+    return (
 
-    return(
         <div className={styles.mainDiv} style={{ overflow: "scroll", maxHeight: "500px" }}>
             {
                 items.map((item, index) => {
+                        console.log("ITEMS LENGTH", items.length);
+
                     return (
-                        <Item key={index} item={item.item} id={item.id} date={item.date} value={item.value} deadline={item.deadline} itemDelete={() => itemDelete(item.id)} itemDone={()=> itemDone(item.id)}/>
+                          <Item key={index} item={item.item} id={item.id} date={item.date} value={item.value} deadline={item.deadline} itemDelete={() => itemDelete(item.id)} itemDone={() => itemDone(item.id)} />
                     )
                 })
             }
